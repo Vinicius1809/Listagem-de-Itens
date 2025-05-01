25abd796e1daa991d358e911c49ad87c433f7d68
Vinicius Henrique <viniciushkzz3@gmail.com>
2025-05-01 19:56:14 -0300

    Criando o botão de remover requisitos

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let requisitos = [];
+
 function adicionarrequisito() {
   const inputrequisito = document.getElementById("inputrequisito");
   // Criei uma nova variavel, que irá buscar em meu HTML o input com ID input requisito
@@ -9,15 +11,41 @@ function adicionarrequisito() {
   } else {
     const mensagem = "Requisito adicionado com sucesso";
     document.getElementById("mensagem").textContent = mensagem;
-    const listarequisitos = document.getElementById("listarequisitos"); // Criamos essa variavel para adicionar uma (LI)
-    const novorequisito = document.createElement("li");
 
-    novorequisito.textContent = requisito;
-
-    listarequisitos.appendChild(novorequisito); // esse comonado é para localizar onde a LI será chamada
-    // Que será dentro da nossa UL com o id listarequisitos
-  } // Nesse (IF) estou dizendo que se o meu campo de pesquisa estiver vazio, enviar um alerta de erro.
-  // Se não estiver vazio, adicionar a tarefa desejada
+    requisitos.push(requisito);
+    renderizarrequisitos();
+  }
 
   inputrequisito.value = ""; // Limpa o meu input após eu escrever o meu requisito e adicionar
 }
+
+function renderizarrequisitos() {
+  const listarequisitos = document.getElementById("listarequisitos"); // Criamos essa variavel para adicionar uma (LI)
+  listarequisitos.innerHTML = "";
+
+  // for (iterador(inicio), condição(final), frequencia)
+
+  for (let i = 0; i < requisitos.length; i++) {
+    // i++ é igual a i + 1
+    const novorequisito = document.createElement("li");
+
+    novorequisito.textContent = requisitos[i];
+
+    let botaoRemover = document.createElement("button");
+    botaoRemover.className = "remover";
+    botaoRemover.textContent = "Remover";
+    botaoRemover.onclick = () => removerRequisito(i);
+
+    novorequisito.appendChild(botaoRemover);
+
+    listarequisitos.appendChild(novorequisito); // esse comonado é para localizar onde a LI será chamada
+    // Que será dentro da nossa UL com o id listarequisitos
+    // Nesse (IF) estou dizendo que se o meu campo de pesquisa estiver vazio, enviar um alerta de erro.
+    // Se não estiver vazio, adicionar a tarefa desejada
+  }
+}
+
+function removerRequisito(i) {
+  requisitos.splice(i, 1);
+  renderizarrequisitos();
+} //requisitos.splice irá remover um requisito da minha lista
